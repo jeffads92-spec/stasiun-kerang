20aef341071cc6d4162df784c61c878b8bb59c6d
jeffads92-spec <jeffads92@gmail.com>
2026-01-06 21:36:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-// index.php
-readfile('index.html');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$file = __DIR__ . '/index.html';
+if (file_exists($file)) {
+    header('Content-Type: text/html');
+    readfile($file);
+} else {
+    http_response_code(404);
+    echo 'File not found. Current dir: ' . __DIR__;
+}
 ?>
